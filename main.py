69dc082fb9da3b96d46ca805b5176f626675f289
friedrich groestlinger <friedrich.groestlinger@gmail.com>
2014-02-13 23:46:45 +0100

    basic pyside window

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+from PySide import QtGui
+import sys
+
+
+class MainWindow(QtGui.QMainWindow):
+	
+	def __init__(self):
+		super(MainWindow, self).__init__()
+		self.initUI()
+		self.show()
+
+	def initUI(self):
+		self.setWindowTitle('basic window')
+
+
+
+def main():
+	app = QtGui.QApplication(sys.argv)
+	mw = MainWindow()
+	sys.exit(app.exec_())
+
+
 if __name__ == '__main__':
-	print('hello world')
+	main()
